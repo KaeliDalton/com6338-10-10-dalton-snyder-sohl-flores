edcc12d8825cd2c8e0ef3434a667b213fcf3a53e
Ian Snyder-Sohl <spitball@Snyder-Sohl.hitronhub.home>
2023-04-10 14:02:00 -0500

    resubmitting pr....hope this doesnt break anything?

--- news.js
+++ news.js
@@ -1,16 +1,13 @@
 const searchForm = document.querySelector('.search-form');
 const searchBtn = document.querySelector('#searchBtn');
 const searchResults = document.createElement('section');
-searchResults.classList.add('search-results');
 document.body.appendChild(searchResults);
 
 searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+    event.preventDefault(); // prevent the default behavior of form submission
     const query = document.querySelector('#search-input').value;
     const apiKey = 'pub_202668307aded72323ad04d186950cf68ece4';
-    const apiUrl = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${query}&country=us`; //displays news data from the US only.
-
-    const nullImageSource = "https://picsum.photos/300/200"
+    const apiUrl = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${query}`;
 
     try {
         const response = await fetch(apiUrl);
@@ -19,30 +16,12 @@ searchForm.addEventListener('submit', async (event) => {
         searchResults.innerHTML = '';
         data.results.forEach(article => {
             const articleDiv = document.createElement('div');
-            articleDiv.classList.add('card'); // add card styling
-            let imageSrc = article.image_url;
-            if (imageSrc === null) {
-                imageSrc = nullImageSource;
-            }
-            articleDiv.innerHTML = `
-                <h2>${article.title}</h2>
-              </div>
-              <img src="${imageSrc}" alt="${article.title}">
-                <p class="card-text">${article.description ? article.description : "Description not found"}</p>
-                <a href="${article.link}" class="card-link" target="_blank">Read more</a>
-              </div>
-            `;
+            articleDiv.innerHTML = `<h3>${article.title}</h3><p>${article.summary}</p><a href="${article.link}" target="_blank">Read more</a>`;
             searchResults.appendChild(articleDiv);
         });
-
-        // hide top stories section
-        const topStoriesSection = document.querySelector('.top-stories');
-        topStoriesSection.style.display = 'none';
-
-        // insert search results above top stories
-        topStoriesSection.insertAdjacentElement('beforebegin', searchResults);
 
     } catch (error) {
         console.log(error);
     }
 });
+
