99f61aabad6e7e46aa57fefce38880a5c10cc12f
Ian Snyder-Sohl <spitball@Snyder-Sohl.hitronhub.home>
2023-04-10 14:39:17 -0500

    refactored JS code: country-spec search, placeholder pics for when pics not returned, displays description instea d of summary due to summary always returning null, including ternary operator to add message if no news description returns, removes top stories section when search is submitted.

--- news.js
+++ news.js
@@ -1,13 +1,16 @@
 const searchForm = document.querySelector('.search-form');
 const searchBtn = document.querySelector('#searchBtn');
 const searchResults = document.createElement('section');
+searchResults.classList.add('search-results');
 document.body.appendChild(searchResults);
 
 searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault(); // prevent the default behavior of form submission
+    event.preventDefault();
     const query = document.querySelector('#search-input').value;
     const apiKey = 'pub_202668307aded72323ad04d186950cf68ece4';
-    const apiUrl = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${query}`;
+    const apiUrl = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${query}&country=us`; //displays news data from the US only.
+
+    const nullImageSource = "https://picsum.photos/300/200"
 
     try {
         const response = await fetch(apiUrl);
@@ -16,12 +19,30 @@ searchForm.addEventListener('submit', async (event) => {
         searchResults.innerHTML = '';
         data.results.forEach(article => {
             const articleDiv = document.createElement('div');
-            articleDiv.innerHTML = `<h3>${article.title}</h3><p>${article.title}</p><a href="${article.link}" target="_blank">Read more</a>`;
+            articleDiv.classList.add('card'); // add card styling
+            let imageSrc = article.image_url;
+            if (imageSrc === null) {
+                imageSrc = nullImageSource;
+            }
+            articleDiv.innerHTML = `
+                <h2>${article.title}</h2>
+              </div>
+              <img src="${imageSrc}" alt="${article.title}">
+                <p class="card-text">${article.description ? article.description : "Description not found"}</p>
+                <a href="${article.link}" class="card-link" target="_blank">Read more</a>
+              </div>
+            `;
             searchResults.appendChild(articleDiv);
         });
+
+        // hide top stories section
+        const topStoriesSection = document.querySelector('.top-stories');
+        topStoriesSection.style.display = 'none';
+
+        // insert search results above top stories
+        topStoriesSection.insertAdjacentElement('beforebegin', searchResults);
 
     } catch (error) {
         console.log(error);
     }
 });
-
